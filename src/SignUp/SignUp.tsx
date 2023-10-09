80ebd927222077c469a9b918c937eaa498ccecbf
Abdur1R <abdurrahmanshaik111@gmail.com>
2023-10-09 21:49:04 +0530

    OnSubmit Changes!

--- src/SignUp/SignUp.tsx
+++ src/SignUp/SignUp.tsx
@@ -1,11 +1,11 @@
 import { useForm } from "antd/es/form/Form";
 import React from "react";
-import { Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import "./SignUp.scss";
 // import BackgroundImg from "../GIFs/background-img.gif";
 
 const FormItem = ({ label, margin, padding }: { label: string, margin?: string, padding?: string }) => (
-    <Form.Item style={{ margin: margin, padding: padding }} label={label}>
+    <Form.Item style={{ margin: margin, padding: padding }} label={label} name={label}>
         <Input />
     </Form.Item >
 );
@@ -13,12 +13,23 @@ const FormItem = ({ label, margin, padding }: { label: string, margin?: string, 
 const SignUp = () => {
     const [form] = useForm();
 
+    const onSubmit = (values: any) => {
+        console.log("values", values);
+        console.log("FieldValues", form.getFieldsValue());
+        sessionStorage.setItem("userData", values);
+    };
+
     return (
         <div className="sign-up">
-            <Form style={{ padding: "300px 300px 400px 300px" }} form={form}>
-                <FormItem label="Email" margin="0px 0px 20px 0px" />
-                <FormItem label="User Name" />
-                <FormItem label="Password" />
+            <Form className="formStyles" form={form} onFinish={onSubmit}>
+                <FormItem label="Email" margin="20px 0px 0px 0px" />
+                <FormItem label="User Name" margin="40px 0px 0px 0px" />
+                <FormItem label="Password" margin="40px 0px 0px 0px" />
+                <Form.Item style={{ marginTop: "70px" }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
             </Form>
         </div>
     )
