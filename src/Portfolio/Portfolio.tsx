1dee8de402483573fa26f83800e0690b55a0ff78
Abdur1R <abdurrahmanshaik111@gmail.com>
2024-06-24 00:54:38 +0530

    DarkMode for Portfolio!

--- src/Portfolio/Portfolio.tsx
+++ src/Portfolio/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
 import SideBar from "./SideBar";
 import "./PortFolio.scss";
 import ProfilePic from "../Pictures/pic.png";
@@ -8,14 +8,27 @@ import EducationSection from "./EducationSection";
 import AboutSection from "./AboutSection";
 import SkillSection from "./SkillSection";
 import LinkSection from "./LinkSection";
+import { Switch } from "antd";
 
-const PortFolio = () => {
+const PortFolio: React.FC = (): ReactElement => {
+    const [state, updateState] = useState<any>({ modeSwitch: false });
+
+    const ModeSwitchChange = (enabled: boolean) => {
+        updateState((prev: any) => ({ ...prev, modeSwitch: enabled }));
+    };
+
     return (
         <div className="portFolio">
             <div className="portfolio-anchor">
                 <div>
                     <img src={ProfilePic} alt="Profile Pic" className="profilePic" />
                 </div>
+                <Switch
+                    style={{ backgroundColor: state.modeSwitch ? "green" : "grey" }}
+                    size="default"
+                    onChange={ModeSwitchChange}
+                    checked={state.modeSwitch}
+                />
                 <SideBar
                     menuList={[
                         {
@@ -51,38 +64,44 @@ const PortFolio = () => {
                     ]}
                 />
             </div>
-            <div className="portfolio-body">
+            <div className={state.modeSwitch ? "portfolio-body darkMode" : "portfolio-body"}>
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"About"}
                     body={<AboutSection />}
                     subHead={""}
                     id={"/portfolio/about"}
                 />
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"Professional Experience (2+ years)"}
                     body={<ExperienceSection />}
                     subHead={"Product Development Engineer - 1 , PHENOM"}
                     id={"/portfolio/experience"}
                 />
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"Education"}
                     body={<EducationSection />}
                     subHead={""}
                     id={"/portfolio/education"}
                 />
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"Skills"}
                     body={<SkillSection />}
                     subHead={""}
                     id={"/portfolio/skills"}
                 />
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"Links"}
                     body={<LinkSection />}
                     subHead={""}
                     id={"/portfolio/links"}
                 />
                 <SectionBody
+                    darkMode={state.modeSwitch}
                     head={"Contact"}
                     body={<>+919866868600 | abdurrahmanshaik111@gmail.com | Hyderabad India</>}
                     subHead={""}
--- src/Portfolio/SectionBody.tsx
+++ src/Portfolio/SectionBody.tsx
@@ -1,19 +1,19 @@
-import React, { ReactComponentElement, ReactElement } from "react";
+import { ReactElement } from "react";
 
-const SectionBody = ({ head, body, subHead, id }: { head: string, body: ReactElement<any, any>, subHead: string, id: string }) => (
+const SectionBody = ({ head, body, subHead, id, darkMode }: { head: string, body: ReactElement<any, any>, subHead: string, id: string, darkMode: boolean }) => (
     <div className="section" id={id}>
         {head &&
-            <div className="head">
+            <div className={darkMode ? "head darkMode" : "head"}>
                 {head}
             </div>
         }
         {subHead &&
-            <div className="subHead">
+            <div className={darkMode ? "subHead darkMode" : "subHead"}>
                 {subHead}
             </div>
         }
         {body &&
-            <div className="body">
+            <div className={darkMode ? "body darkMode" : "body"}>
                 {body}
             </div>
         }
